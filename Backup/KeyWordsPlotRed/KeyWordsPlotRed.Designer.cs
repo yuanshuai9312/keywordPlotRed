7e46770c01f4141eb33979d01288dd975cad0916
yuan.shuai <yuanshuai9312@gmail.com>
2020-07-02 23:53:08 +0900

    first commit

---
+++ Backup/KeyWordsPlotRed/KeyWordsPlotRed.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace KeyWordsPlotRed
+{
+    partial class KeyWordsPlotRed
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.keyWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plotRed = new System.Windows.Forms.Button();
+            this.unfold = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.unfold);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12,12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309,196);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "内容区域";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.plotRed);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.keyWord);
+            this.groupBox2.Location = new System.Drawing.Point(13,214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(308,78);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "操作区域";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(5,14);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(299,148);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // keyWord
+            // 
+            this.keyWord.Location = new System.Drawing.Point(59,31);
+            this.keyWord.Name = "keyWord";
+            this.keyWord.Size = new System.Drawing.Size(183,21);
+            this.keyWord.TabIndex = 0;
+            this.keyWord.TextChanged += new System.EventHandler(this.keyWord_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3,35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53,12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "关键字：";
+            // 
+            // plotRed
+            // 
+            this.plotRed.Location = new System.Drawing.Point(252,31);
+            this.plotRed.Name = "plotRed";
+            this.plotRed.Size = new System.Drawing.Size(45,23);
+            this.plotRed.TabIndex = 2;
+            this.plotRed.Text = "描红";
+            this.plotRed.UseVisualStyleBackColor = true;
+            this.plotRed.Click += new System.EventHandler(this.plotRed_Click);
+            // 
+            // unfold
+            // 
+            this.unfold.Location = new System.Drawing.Point(253,167);
+            this.unfold.Name = "unfold";
+            this.unfold.Size = new System.Drawing.Size(45,23);
+            this.unfold.TabIndex = 3;
+            this.unfold.Text = "打开";
+            this.unfold.UseVisualStyleBackColor = true;
+            this.unfold.Click += new System.EventHandler(this.unfold_Click);
+            // 
+            // KeyWordsPlotRed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F,12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333,304);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "KeyWordsPlotRed";
+            this.Text = "在RichTextBox中实现关键字描红";
+            this.Load += new System.EventHandler(this.KeyWordsPlotRed_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button plotRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox keyWord;
+        private System.Windows.Forms.Button unfold;
+    }
+}
+
---
+++ KeyWordsPlotRed/KeyWordsPlotRed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;//需引用命名空间Using System.IO
+
+namespace KeyWordsPlotRed
+{
+    public partial class KeyWordsPlotRed : Form
+    {
+        public KeyWordsPlotRed()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyWordsPlotRed_Load(object sender,EventArgs e)
+        {
+         
+        }
+        private void FetchFile(string path)
+        {
+            StreamReader UTF_8Reader = new StreamReader(path,Encoding.Default);//实现一个 TextReader，使其以一种特定的编码从字节流中读取字符。
+            richTextBox1.Text = UTF_8Reader.ReadToEnd();//在RichTextBox控件中显示从字节流中读取的字符
+        }
+
+        private void unfold_Click(object sender,EventArgs e)
+        {
+        }
+
+        private int flag = 0;//定义一个int型的标识符
+        private void plotRed_Click(object sender,EventArgs e)
+        {
+            if((flag = richTextBox1.Text.IndexOf(keyWord.Text,flag)) == -1)//当文件中不存在要搜索的关键字时
+            {
+                MessageBox.Show("没有要查找的结果","提示信息",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);//弹出对应的信息提示
+                keyWord.Clear();//清空文本框中的内容
+                flag = 0;//重新为flag赋值
+            }
+            else//当在文件中存在对应的关键字时
+            {
+                richTextBox1.Select(flag,keyWord.Text.Length);//在RichTextBox控件中搜索关键字
+                flag = flag + keyWord.Text.Length;//递增标识查询关键字的初始长度
+                richTextBox1.SelectionColor = Color.Red;//设定关键字为红色
+            }
+        }
+
+        private void keyWord_TextChanged(object sender,EventArgs e)
+        {
+            if(keyWord.Text == "" || keyWord.Text == null)//当文本框中不存在内容或者内容为空时
+            {
+                plotRed.Enabled = false;//设定“描红”按钮为不可用状态
+            }
+            else//当文本框中存在内容时
+            {
+                plotRed.Enabled = true;//设定“描红”按钮为可用状态
+            }
+        }   
+    }
+}
